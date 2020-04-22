2b211cbd0d0775e69ca91d9e00f5b4bb2ff2d465
RamyasreeChakka <ramya@microsoft.com>
2020-04-21 21:01:03 -0700

    Test RG create

--- scripts/testScript.sh
+++ scripts/testScript.sh
@@ -1,1 +1,5 @@
 echo "Welcome to GitHub Actions!"
+testResourceGroup=$(echo "testRG`date +"%Y%m%d%H%M%S"`")
+echo "Test resource group: ${testResourceGroup}"
+az account set -s 44d01367-c909-4ddc-94ef-9c4a4b34ed23
+az group create --name ${testResourceGroup} --location westus2
